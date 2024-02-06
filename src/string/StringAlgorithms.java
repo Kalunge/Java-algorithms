237682cc8dbac14ed26271a26b62429a862bcb1e
Titus Muthomi <muthomititus+github@gmail.com>
2024-02-06 14:04:00 +0300

    Addd normalization

--- src/string/StringAlgorithms.java
+++ src/string/StringAlgorithms.java
@@ -12,16 +12,19 @@ public class StringAlgorithms {
 
     public static boolean isPasswordComplex(String str) {
 //        .chars method returns a stream of characters
-        return str.chars().anyMatch(Character::isUpperCase)
-                && str.chars().anyMatch(Character::isLowerCase)
-                && str.chars().anyMatch(Character::isDigit);
+        return str.chars().anyMatch(Character::isUpperCase) && str.chars().anyMatch(Character::isLowerCase) && str.chars().anyMatch(Character::isDigit);
+    }
+
+    public static String normalizeString(String str) {
+        return str.toLowerCase().trim().replace(",", "");
     }
 
     public static void main(String[] args) {
-        System.out.println(isUpperCase("MUTHOMI"));
-        System.out.println(isUpperCase("kalunge"));
-        System.out.println(isLower("titus"));
-        System.out.println(isPasswordComplex("Muthomiz2"));
-        System.out.println(isPasswordComplex("Muthomiz"));
+//        System.out.println(isUpperCase("MUTHOMI"));
+//        System.out.println(isUpperCase("kalunge"));
+//        System.out.println(isLower("titus"));
+//        System.out.println(isPasswordComplex("Muthomiz2"));
+//        System.out.println(isPasswordComplex("Muthomiz"));
+        System.out.println(normalizeString(" Hello There BUDDY , "));
     }
 }
